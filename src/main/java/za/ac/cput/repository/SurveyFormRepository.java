abd087ddca533d9cb005e35dd92e0399b4180fc2
AneleNqabeni220403635 <220403635@mycput.ac.za>
2025-06-03 17:56:52 +0200

    survey form added with Jpa in the repository package

---
+++ src/main/java/za/ac/cput/repository/SurveyFormRepository.java
@@ -0,0 +1,9 @@
+package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.SurveyForm;
+
+@Repository
+public interface SurveyFormRepository extends JpaRepository<SurveyForm,Long> {
+}
