dd1ba66623e35ac334fb8411ee4f4f168fd38b29
AneleNqabeni220403635 <220403635@mycput.ac.za>
2025-06-03 23:39:14 +0200

    model package added + enum class(FavouriteFood)

---
+++ src/main/java/za/ac/cput/model/FavouriteFood.java
@@ -0,0 +1,31 @@
+package za.ac.cput.model;
+
+import java.sql.SQLOutput;
+import java.util.Objects;
+
+public enum FavouriteFood {
+    PIZZA("Pizza"),
+    PASTA("Pasta"),
+    PapAndWors("Pap and wors"),
+    Other("Other");
+
+    private final String foodName;
+
+    FavouriteFood(String foodName) {
+        this.foodName = foodName;
+    }
+    public String getFood() {
+        return foodName;
+    }
+
+    public static FavouriteFood fromKey(String key) {
+        for (FavouriteFood food : values()) {
+            System.out.println(food.foodName);
+            if (Objects.equals(food.foodName, key.trim())) {
+                return food;
+            }
+        }
+        throw new IllegalArgumentException("Unknown key:" + key);
+}
+
+}
