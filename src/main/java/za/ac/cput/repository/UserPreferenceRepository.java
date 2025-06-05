834858bd0e6889bec2e62aca1abe054bd3f1b931
AneleNqabeni220403635 <220403635@mycput.ac.za>
2025-06-05 16:06:58 +0200

    more debugging

--- src/main/java/za/ac/cput/repository/UserPreferenceRepository.java
+++ src/main/java/za/ac/cput/repository/UserPreferenceRepository.java
@@ -1,15 +1,9 @@
 package za.ac.cput.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.model.UserPreference;
 
 @Repository
-public class UserPreferenceRepository {
-    public UserPreference save(UserPreference userPreference) {
-        int counter = 1;
-
-        System.out.println("Saving UserPreference: " + userPreference + ", ID: " + counter);
-        return userPreference;
-}
-
+public interface UserPreferenceRepository extends JpaRepository<UserPreference,String>{
 }
